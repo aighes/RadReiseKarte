5a715b2c0bfad299e841c6ee11f2a9b962a1bb62
aighes <info@aighes.de>
2013-05-07 23:32:07 +0200

    tweaking overviewmap +place=country name:en/int_name for name-tags outside several coutries

--- #proof_style.sh
+++ #proof_style.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-java -jar -Xmx6000M ./bin/mkgmap.jar --style-file=./resources/style_rrk --check-styles
-java -jar -Xmx6000M ./bin/mkgmap.jar --help=options > help.txt
-java -jar -Xmx6000M ./bin/mkgmap.jar ./resources/rrk_typ.txt
+java -jar ./bin/mkgmap.jar --read-config=./resources/style_rrk_ov/options --check-styles
+java -jar ./bin/mkgmap.jar ./resources/rrk_typ.txt
 rm ./rrk_typ.typ
 rm ./osmmap.img
 rm ./osmmap.tdb
-read -p "Press any Key to exit script"
-exit
