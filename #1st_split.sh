e487f5c16982eadd5e15c559731f440ecff7ccd1
aighes <info@aighes.de>
2013-11-19 21:52:11 +0100

    update

--- #1st_split.sh
+++ #1st_split.sh
@@ -2,9 +2,9 @@
 threads=6
 maxnodes=1200000
 
-name=Turkey
+name=Oman
 mapid=1010
-java -jar ./bin/splitter.jar --max-threads=$threads --stop-after=split --write-kml=./resources/list/$name".kml" --status-freq=0 --max-nodes=$maxnodes --mapid=$mapid"0001" --polygon-file=./resources/poly/$name.poly ./data/planet.o5m ./data/srtm.o5m
+java -jar ./bin/splitter.jar --max-threads=$threads --stop-after=split --write-kml=./resources/kml/$name".kml" --status-freq=0 --max-nodes=$maxnodes --mapid=$mapid"0001" --polygon-file=./resources/poly/$name.poly ./data/planet.o5m ./data/srtm.o5m
 sort areas.list | sed '/^$/d' | sed '/^\#/d' > ./resources/list/$name.list
 rm areas.list
 rm areas.poly
